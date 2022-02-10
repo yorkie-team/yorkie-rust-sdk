1689dbfc494d8b518ab407716ff1d0d5f24dead9
dc7303 <dc7303@gmail.com>
2022-02-10 23:13:54 +0900

    Change bson_key to combined_key

--- src/document/key.rs
+++ src/document/key.rs
@@ -1,14 +1,14 @@
 use std::fmt;
 
-const BSON_SPLITTER: &str = "$";
+const SPLITTER: &str = "$";
 const TOKEN_LEN: usize = 2;
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct InvalidBSONKeyError;
+pub struct InvalidCombinedKeyError;
 
-impl fmt::Display for InvalidBSONKeyError {
+impl fmt::Display for InvalidCombinedKeyError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "invalid bson key")
+        write!(f, "invalid combined key")
     }
 }
 
@@ -20,11 +20,11 @@ pub struct Key {
 }
 
 impl Key {
-    /// from_bson_key creates an instance of Key from the received bsonKey.
-    pub fn from_bson_key(bson_key: &str) -> Result<Key, InvalidBSONKeyError> {
-        let mut splits = bson_key.split(BSON_SPLITTER).collect::<Vec<_>>();
+    /// from_combined_key creates an instance of Key from the received combined key.
+    pub fn from_combined_key(combined_key: &str) -> Result<Key, InvalidCombinedKeyError> {
+        let mut splits = combined_key.split(SPLITTER).collect::<Vec<_>>();
         if splits.len() != TOKEN_LEN {
-            return Err(InvalidBSONKeyError);
+            return Err(InvalidCombinedKeyError);
         }
 
         let collection = splits[0].to_string();
@@ -35,9 +35,9 @@ impl Key {
         })
     }
 
-    /// bson_key returns the string of this key.
-    pub fn bson_key(&self) -> String {
-        format!("{}{}{}", self.collection, BSON_SPLITTER, self.document)
+    /// combined_key returns the string of this key.
+    pub fn combined_key(&self) -> String {
+        format!("{}{}{}", self.collection, SPLITTER, self.document)
     }
 
     pub fn collection(&self) -> &str {
@@ -54,24 +54,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn from_bson_key() {
+    fn from_combined_key() {
         // create success
-        let key = Key::from_bson_key("collection$document").unwrap();
+        let key = Key::from_combined_key("collection$document").unwrap();
         assert_eq!(key.collection, "collection");
         assert_eq!(key.document, "document");
 
-        // invalid bson
-        let err = Key::from_bson_key("collection").unwrap_err();
-        assert_eq!(err, InvalidBSONKeyError);
-        let err = Key::from_bson_key("collection$document$bb").unwrap_err();
-        assert_eq!(err, InvalidBSONKeyError);
+        // invalid combined key
+        let err = Key::from_combined_key("collection").unwrap_err();
+        assert_eq!(err, InvalidCombinedKeyError);
+        let err = Key::from_combined_key("collection$document$bb").unwrap_err();
+        assert_eq!(err, InvalidCombinedKeyError);
     }
 
     #[test]
-    fn bson_key() {
+    fn combined_key() {
         // create success
-        let bson_key = "collection$document";
-        let key = Key::from_bson_key(bson_key).unwrap();
-        assert_eq!(key.bson_key(), bson_key);
+        let combined_key = "collection$document";
+        let key = Key::from_combined_key(combined_key).unwrap();
+        assert_eq!(key.combined_key(), combined_key);
     }
 }
