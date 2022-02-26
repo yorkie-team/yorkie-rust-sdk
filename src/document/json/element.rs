f238836408cf3cf6f425537a7753a96b6bd875a0
Changhui Lee <iam@xo.dev>
2022-02-26 22:13:06 +0900

    fix: infinite self call

--- src/document/json/element.rs
+++ src/document/json/element.rs
@@ -2,7 +2,7 @@ use crate::document::time::ticket::Ticket;
 
 pub trait Element {
     fn to_string(&self) -> String;
-    fn clone(&self) -> Box<dyn Element>;
+    fn deepcopy(&self) -> Box<dyn Element>;
     fn created_at(&self) -> Ticket;
     fn moved_at(&self) -> Option<Ticket>;
     fn set_moved_at(&self, ticket: Ticket);
@@ -12,6 +12,6 @@ pub trait Element {
 
 impl Clone for Box<dyn Element> {
     fn clone(&self) -> Box<dyn Element> {
-        self.clone()
+        self.deepcopy()
     }
 }
