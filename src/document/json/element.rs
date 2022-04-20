85f19a6b151a7c1a3de5319755f26a0b49d47191
changhui lee <2173789+blurfx@users.noreply.github.com>
2022-04-20 21:35:42 +0900

    Update src/document/json/element.rs
    Co-authored-by: Dongcheol Choe <40932237+dc7303@users.noreply.github.com>

--- src/document/json/element.rs
+++ src/document/json/element.rs
@@ -5,7 +5,7 @@ pub trait Element {
     fn deepcopy(&self) -> Box<dyn Element>;
     fn created_at(&self) -> Ticket;
     fn moved_at(&self) -> Option<Ticket>;
-    fn set_moved_at(&self, ticket: Ticket);
+    fn set_moved_at(&mut self, ticket: Ticket);
     fn removed_at(&self) -> Option<Ticket>;
     fn remove(&mut self, ticket: Ticket) -> bool;
 }
